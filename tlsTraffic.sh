fba79edc241324aa65e5ca5336ebffbdef7c3959
System Administrator <root@Eriks-MacBook-Pro-3.local>
2019-06-06 14:47:31 -0700

    dding tls traffic generator

---
+++ tlsTraffic.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+#set arguements
+NAME=
+
+#Loop a bunch of interesting things that use encrypted traffic
+
+while true; do
+#AWS API call on port 443 using TLS to show user info
+#JSON is always the preferred format as it visualizes easier
+aws iam get-user  --output json
+sleep 5
+#AWS API call on port 443 using TLS to show user info
+aws ec2 describe-vpcs --output json
+sleep 5
+#Grab EICAR first as binary then as text
+curl --output /dev/null https://secure.eicar.org/eicar.com
+curl --output /dev/null https://secure.eicar.org/eicar.com.txt
+sleep 5
+#TLS version of TestmyIDS.com
+curl --output /dev/null https://evebox.org/files/testmyids.com
+
+done
