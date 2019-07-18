e2dff3c70e482262d4d13d9ad69ac586cabf668b
Erik Freeland <erik.freeland@gmail.com>
2019-07-18 11:12:02 -0700

    Update tlsTraffic.sh

--- tlsTraffic.sh
+++ tlsTraffic.sh
@@ -9,16 +9,21 @@ NAME=
 while true; do
 #AWS API call on port 443 using TLS to show user info
 #JSON is always the preferred format as it visualizes easier
+#expected output here is a auth error
+#this will fail on Azure of course
 aws iam get-user  --output json
 sleep 5
 #AWS API call on port 443 using TLS to show user info
-aws ec2 describe-vpcs --output json --region us-east-1
-sleep 5
+#aws ec2 describe-vpcs --output json --region us-east-1
+#sleep 5
 #Grab EICAR first as binary then as text
 curl --output /dev/null https://secure.eicar.org/eicar.com
-curl --output /dev/null https://secure.eicar.org/eicar.com.txt
 sleep 5
 #TLS version of TestmyIDS.com
 curl --output /dev/null https://evebox.org/files/testmyids.com
 sleep 5
+#Download Google Homepage via TLS
+curl --output /dev/null https://www.google.com
+sleep 5
+curl --output /dev/null https://www.espn.com
 done
