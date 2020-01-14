97f2fa4690e1e98b9a157e323cf01e93bd8509ae
Erik Freeland <erik.freeland@gmail.com>
2020-01-14 13:40:44 -0800

    Update loadGenerateAWS.sh

--- loadGenerateAWS.sh
+++ loadGenerateAWS.sh
@@ -8,6 +8,6 @@ NAME=
 
 while true; do
 #Grab 100MB file on S3
-wget -q -O /dev/null https://tlsELB-c03c5d178d8b2b95.elb.us-east-1.amazonaws.com/100MB.zip
+wget -q -O /dev/null https://tlsELB-c03c5d178d8b2b95.elb.us-east-1.amazonaws.com/100MB.zip --no-check-certificate
 sleep $[ ( $RANDOM % 5 )  + 1 ]s
 done
