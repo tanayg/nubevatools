128ae7d694731f787a8af0d1c37a70f0bd83f655
System Administrator <root@Eriks-MacBook-Pro-3.local>
2020-03-09 12:30:56 -0700

    Cleanup

--- tlsgen.ps1
+++ tlsgen.ps1
@@ -1,17 +1,17 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#Powershell version of TLS generator. 
+#Powershell version of TLS generator.
 while ($true) {
 $progressPreference = 'silentlyContinue'
 #Grab EICAR first as binary then as text
 invoke-webrequest -Uri https://secure.eicar.org/eicar.com -OutFile $NULL | Out-Null
-start-sleep -seconds 5
+start-sleep -seconds 25
 #TLS version of TestmyIDS.com
-invoke-webrequest -Uri https://evebox.org/files/testmyids.com -OutFile $NULL | Out-Null
-start-sleep -seconds 5
+invoke-webrequest -Uri https://nubevalabs.s3.amazonaws.com/testmyids.txt -OutFile $NULL | Out-Null
+start-sleep -seconds 25
 #Download Google Homepage via TLS
 invoke-webrequest -Uri https://www.google.com  -OutFile $NULL | Out-Null
-start-sleep -seconds 5
+start-sleep -seconds 25
 #Download ESPN Homepage via TLS
-invoke-webrequest -Uri https://www.espn.com -OutFile $NULL | Out-Null
-start-sleep -seconds 5
+invoke-webrequest -Uri https://www.bbc.com -OutFile $NULL | Out-Null
+start-sleep -seconds 25
 }
