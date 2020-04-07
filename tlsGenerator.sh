1768151778b0765b80144beeedd210baffef4955
Tanay Gottigundala <tgottigundala@gmail.com>
2020-04-07 15:20:26 -0700

    Create tlsGenerator.sh

---
+++ tlsGenerator.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+#set arguements
+NAME=
+
+#Loop a bunch of interesting things that use encrypted traffic
+
+while true; do
+#Grab EICAR as binary
+wget -q -O /dev/null https://secure.eicar.org/eicar.com
+sleep $[ ( $RANDOM % 10 )  + 15 ]s
+#TLS version of TestmyIDS.com
+wget -q -O /dev/null https://nubevalabs.s3.amazonaws.com/testmyids.txt
+sleep $[ ( $RANDOM % 10 )  + 15 ]s
+#Download Google Homepage via TLS 1.3
+wget -q -O /dev/null https://www.google.com
+sleep $[ ( $RANDOM % 10 )  + 15 ]s
+#Download BBC Homepage via TLS
+wget -q -O /dev/null https://www.bbc.com
+sleep $[ ( $RANDOM % 10 )  + 15 ]s
+#Download Malware Sample from Fireeye. Original URL http://fedeploycheck.fireeye.com/appliance-test/test-infection.exe
+wget -q -O /dev/null https://nubevalabs.s3.amazonaws.com/samples/malware-test-infection.exe
+sleep $[ ( $RANDOM % 10 )  + 15 ]s
+done
+
+#Saved for Later
+#Clone Git Repository
+#git clone https://github.com/nubevalabs/demorepo.git /tmp/demorepo
+#sudo rm -r /tmp/demorepo
+
+#This is a SQL injection attempt. The 1=1 is an indicatior. 
+#http://test.com/items.php?id=2 and 1=1
+
+#AWS API call on port 443 using TLS to show user info
+#JSON is always the preferred format as it visualizes easier
+#aws iam get-user  --output json
+#AWS API call on port 443 using TLS to show user info
+#aws ec2 describe-vpcs --output json --region us-east-1
